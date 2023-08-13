96600a0473e377a97b9f6c9b1e8f509ade62b75b
Vitor <vitorhugofernandes035@gmail.com>
2023-08-13 02:27:14 -0300

    Gerando um botão com titulo

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,11 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
-public stringintepolation = "Isso é u teste" 
+export class TestComponent  {
 
-public myBindingProperty : string = ''
-public processClick(){
-  alert("Botão em funcionamento")
-}
+  public mostrarTitulo = true;
+  mostrarbotao: string = "Mostrar titulo";
+  tirarbotao: string = "tirar titulo"
+  TituloBotao: Boolean = true;
+  
+  public botaoStatusDeTitulo(){
+    if(this.mostrarTitulo) {
+      this.mostrarTitulo = false;
+    }
+    else {
+      this.mostrarTitulo = true;
+    }
+    this.TituloBotao = !this.TituloBotao;
+  }
+
 }
