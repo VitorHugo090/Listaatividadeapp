f3837920af104a6ff58ae53e9c9b3f0c687e7166
Vitor <vitorhugofernandes035@gmail.com>
2023-08-23 09:41:25 -0300

    Tabelas e botoes implementados

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Category } from '../_models/category';
 
+export const CATEGORY_DATA = [
+  {name: 'Educação', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Saude', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Trabalho', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Outros', guid: 'aaa-bbb-ccc-ddd'},
+  {name: 'Bem estar', guid: 'aaa-bbb-ccc-ddd'}
+]
 
 @Component({
   selector: 'app-category',
@@ -12,13 +19,20 @@ import { Category } from '../_models/category';
 
 export class CategoryComponent {
   public displayedColumns: string[] = ['id', 'name', 'actions'];
-  public dataSource!: Category[];
+  public dataSource: Category[] = CATEGORY_DATA;
 
   public editarCategory(category: Category){
+
+    alert("Olá botao editar testado");
 
   }
 
  public apagarCategory(category: Category){
-  
+  alert("Olá botao apagar testado");
+ }
+
+ public createNewCategory(){
+  alert("Olá botao criar testado");
+
  }
 }
